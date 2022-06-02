5a721c04b4cfc70a2c238986e92d0cf92dd93bb8
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-06-03 00:51:09 +0300

    project preparation

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# books-reading-frontend-
+team project according to the coach for reading books
---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome team!</h1>
+      <p>Here is our app ;)</p>
+      <p>Go Go Go!!!</p>
+    </div>
+  );
+}
+
+export default App;
