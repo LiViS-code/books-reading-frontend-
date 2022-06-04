ee030dee7f4f33c8c085614fc0e1d2eaedda3fd4
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-06-04 14:11:18 +0300

    upd

---
+++ src/redux/api/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const http = axios.create({
+  baseURL: "http://localhost:3000", // address backend server
+  headers: {
+    "Content-type": "application/json",
+  },
+});
