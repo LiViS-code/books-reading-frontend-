2c6ceed2f9d950922a06a7f6bcd96990cb7df5bd
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-06-04 12:06:16 +0300

    routs, fonts, some structure

---
+++ src/App.styled.js
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components';
+import { theme } from './constants/Theme';
+const {
+  spacing,
+  colors: { primaryColor, backgroundColor },
+  fonts: { mainFont },
+  fontSizing: { mainFontSize },
+} = theme;
+export const GlobalStyle = createGlobalStyle`
+  body {
+     width: 100vw;
+    overflow-x: hidden;
+    margin:0;
+    font-family: ${mainFont};
+    font-size:${mainFontSize};
+    color:${primaryColor};
+    background-color:${backgroundColor};
+  }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+button,
+ul,
+ol,
+li {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+ul {
+  padding-left: 0;
+}
+
+img,
+a,
+button {
+  display: block;
+}
+
+a {
+  outline: none;
+}
+
+
+.link {
+  text-decoration: none;
+  font-style: normal;
+  color: inherit;
+}
+
+.list {
+  list-style: none;
+}`;
--- src/constants/Theme.js
+++ src/constants/Theme.js
@@ -1,23 +1,38 @@
 export const theme = Object.freeze({
   colors: {
-    primaryColor: "#242A37",
-    secondaryColor: "#898F9F",
-    accentColor: "#FF6B08",
+    primaryColor: '#242A37',
+    secondaryColor: '#898F9F',
+    accentColor: '#FF6B08',
+    activeColor: '#FFFFFF',
 
     // change according to layout
-    disabledColor: "#666666",
-    outlineColor: "#F7CC07",
-    backgroundColor: "#F7F5F9",
-    backgroundColorForm: "#E3DEE9",
-    borderDisabledColor: "#999999",
-    backgroundDisabledColor: "#cccccc",
-    overlayColor: "#00000088",
-    boxShadow: `4px 4px 8px 0px rgba(34, 60, 80, 0.2)`,
+    disabledColor: '#6D7A8D',
+    outlineColor: '#A6ABB9',
+    backgroundColor: '#F6F7FB',
+    backgroundColorForm: '#F5F7FA',
+    borderDisabledColor: '#999999',
+    borderColor: '#A6ABB9',
+    backgroundDisabledColor: '#cccccc',
+    overlayColor: '#002B2B2B',
+    boxShadow: `0px 1px 2px 0px rgba(#1D1D1B, 0.15)`,
   },
   transition: {
-    timing: "250ms",
+    timing: '250ms',
     timingFunction: `cubic-bezier(0.075, 0.82, 0.165, 1)`,
   },
+  fonts: {
+    mainFont: 'Montserrat',
+    titleFont: 'Abril Fatface',
+    numbersFont: 'Open Sans',
+  },
+  fontSizing: {
+    mainFontSize: '14px',
+  },
+  media: {
+    mobile: '480px',
+    tablet: '768px',
+    desktop: '1280px',
+  },
 
-  spacing: (value) => `${4 * value}px`,
+  spacing: value => `${4 * value}px`,
 });
