bc53d54544d4e8e837d6d385b7a72b9db6c67ae9
KristinaKim6686 <82406962+KristinaKim6686@users.noreply.github.com>
2022-06-13 07:00:42 +0200

    Revert "authSlice"

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants/Theme';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import resumeReducer from './slices/resumeSlice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import storage from 'redux-persist/lib/storage';
-import authSlice from './slices/authSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
-const authPersistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
 
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authSlice),
     resume: resumeReducer,
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  ],
 });
 
 setupListeners(store.dispatch);
-export const persistor = persistStore(store);
 export default store;
