45e77541fc094d078aa33dd41ed756c67866c58c
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-06-16 20:34:10 +0300

    small fif

--- src/components/PublicRoutes.js
+++ src/components/PublicRoutes.js
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from '../redux/selectors/auth-selectors';
-
-export default function PublicRoute({ children, restricted = false }) {
+import AuthView from '../views/AuthView/AuthView';
+export default function PublicRoute({ restricted = false }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
   const shouldRedirect = isLoggedIn && restricted;
 
-  return <>{shouldRedirect ? <Navigate to="library" /> : children}</>;
+  return <>{shouldRedirect ? <Navigate to="library" /> : <AuthView />}</>;
 }
