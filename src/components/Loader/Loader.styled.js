d577da4d268d807c73494e1d5dd1bf04a5ae677d
ValiaStohnii <84042841+ValiaStohnii@users.noreply.github.com>
2022-06-16 14:03:50 +0300

    fix login, add loader

---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: -8px;
+  width: 100vw;
+  height: 100vh;
+  background-color: #282c34;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
